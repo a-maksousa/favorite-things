0be717659e1cf2c0a79f974009772c794f36fe56
Ahmad Maksousa <ah_maksousa@hotmail.com>
2019-09-09 10:41:35 +0300

    Database Connection String

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "key"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+mysqlconnector://root:root@localhost/favorites'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
