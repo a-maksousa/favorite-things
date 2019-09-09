3ba2bbb9cb1d63abba326cde786203a9f0995ef5
Ahmad Maksousa <ah_maksousa@hotmail.com>
2019-09-10 00:32:20 +0300

    use util as helper methods

--- Util.py
+++ Util.py
@@ -1,8 +1,7 @@
-class Response():
-    def success(data={}):
-        data = {'code':1,'data':data}
-        return data
+def success(data={}):
+    data = {'code':1,'data':data}
+    return data
 
-    def failure(msg="Unknown Error"):
-        data = {'code':0,'msg':msg}
-        return data
+def failure(msg="Unknown Error"):
+    data = {'code':0,'msg':msg}
+    return data
