b20fe31b161883b29884350b56bc9dc171d8bdd3
Ahmad Maksousa <ah_maksousa@hotmail.com>
2019-09-09 17:25:00 +0300

    API for GetAllCategories

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,28 @@
 from app import app, db
-from flask import request
+from flask import request, jsonify
 from Util import Response
-from app.models import Categories_Lookup
+from app.models import Categories_Lookup, Favorite
 from datetime import datetime
+import json
 
 # Web API
 
 # Categories
-@app.route('/addCategory',methods=['POST'])
-def addCategory():
+@app.route('/GetAllCategories', methods=['GET'])
+def GetAllCategories():
+    try:
+        lstCategories = Categories_Lookup.query.all()
+        response = []
+        for objCategory in lstCategories:
+            response.append(objCategory.as_dict())
+        return jsonify(Response.success(response))
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(Response.failure())
+
+@app.route('/AddCategory',methods=['POST'])
+def AddCategory():
     try:
         strCategoryTitle = request.form["strCategoryTitle"]
         objCategory = Categories_Lookup.query.filter_by(title = strCategoryTitle).first()
@@ -21,8 +35,8 @@ def addCategory():
         else:
             response = Response.failure("This Category is already exists")
 
+        return jsonify(response)
+
     except Exception as e:
         db.session.rollback()
-        response = Response.failure("Unknown Error")
-
-    return response
+        return jsonify(Response.failure())
