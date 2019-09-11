14f0d3f31b65ef840babad869fe5532068c5bb20
Ahmad Maksousa <ah_maksousa@hotmail.com>
2019-09-12 00:53:17 +0300

    Developement db

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config(object):
     strDevConnectionString = 'mysql+mysqlconnector://root:root@localhost/favoritesdb'
     strProdConnectionString = 'mysql+mysqlconnector://admin:adminadmin@favoritesdb.c21zyakxlmwh.us-east-2.rds.amazonaws.com/favoritesdb'
     SECRET_KEY = os.environ.get('SECRET_KEY') or "key"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or strProdConnectionString
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or strDevConnectionString
     SQLALCHEMY_TRACK_MODIFICATIONS = False
