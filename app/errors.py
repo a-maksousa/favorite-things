b140dcf0092dcc5ae1d27d58be60f35f6976f482
Ahmad Maksousa <ah_maksousa@hotmail.com>
2019-12-10 10:18:07 +0200

    error handlers

--- app/errors.py
+++ app/errors.py
@@ -3,6 +3,18 @@ from flask import jsonify
 from Util import failure
 
 @app.errorhandler(500)
-def page_not_found(error):
+def server_error(error):
     db.session.rollback()
     return jsonify(failure())
+
+@app.errorhandler(400)
+def bad_request(error):
+    db.session.rollback()
+    if error.args[0]:
+        return jsonify(failure("%s is Required" % error.args[0]))
+    return jsonify(failure())
+
+@app.errorhandler(404)
+def not_found(error):
+    db.session.rollback()
+    return jsonify(failure("Incorrect Route Path"))
